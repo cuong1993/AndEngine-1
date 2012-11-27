37728e9a3fe879b9ab901d5682408818b01d5993
zk <ndviettien.zk@gmail.com>
2012-11-28 01:32:42 +0700

    Thêm các hằng số liên quan tới Tốc độ chạy và tốc độ bắn thay đổi 1 số hằng số để phù hợp với Game

--- Tank/src/com/zk/tank/constant/GameConstants.java
+++ Tank/src/com/zk/tank/constant/GameConstants.java
@@ -2,6 +2,11 @@ package com.zk.tank.constant;
 
 public interface GameConstants {
 	//=========================================================//
+	//			hằng số liên quan tới phân giải
+	//=========================================================//
+	public static final int CAMERA_WIDTH = 800;
+	public static final int CAMERA_HEIGHT = 480;
+	//=========================================================//
 	//			hằng số liên quan tới đường dẫn
 	//=========================================================//
 	public static final String ASSET_GRAPHICS = "gfx/";
@@ -25,24 +30,37 @@ public interface GameConstants {
 	public static final int RIGHT = 90;
 	public static final int DOWN = 180;
 	public static final int LEFT = 270;
+	public static final int NONE = -1;
 	//=========================================================//
 	//			hằng số liên quan tới tiled map
 	//=========================================================//
 	/**
-	 * Số ô trên hàng dọc (15)
+	 * Số ô trên hàng dọc (10)
 	 */
-	public static final int TILED_HORIZONTAL = 15;
+	public static final int TILED_HORIZONTAL = 10;
 	/**
-	 * Số ô trên hàng ngang (20)
+	 * Số ô trên hàng ngang (15)
 	 */
-	public static final int TILED_VERTICAL = 20;
-	
+	public static final int TILED_VERTICAL = 15;	
 	/**
 	 * Độ rộng của 1 ô (32px)
 	 */
-	public static final int TILED_WIDHT = 32;
+	public static final int TILED_WIDTH = 48;
 	/**
 	 * Độ cao của 1 ô (32px)
 	 */
-	public static final int TILED_HEIGHT = 32;
+	public static final int TILED_HEIGHT = 48;
+	//=========================================================//
+	//			hằng số liên quan tới tốc độ chạy
+	//=========================================================//
+	public static final float SPEED_SLOW = 1.0f;
+	public static final float SPEED_MEDIUM = 1/50f;
+	public static final float SPEED_FAST = 1/60f;
+	public static final int SPEED_STEP = 48;
+	//=========================================================//
+	//			hằng số liên quan tới tốc độ bắn
+	//=========================================================//
+	public static final float SHOT_SPEED_SLOW = 1.0f;
+	public static final float SHOT_SPEED_MEDIUM = 0.7f;
+	public static final float SHOT_SPEED_FAST = 0.5f;
 }
