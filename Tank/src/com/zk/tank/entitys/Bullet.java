79bb89db415ef98e250fbd03a4bbebae6ea814c0
zk <ndviettien.zk@gmail.com>
2012-11-29 15:55:17 +0700

    Mô tả đối tượng khi gặp va chạm (pending)

--- Tank/src/com/zk/tank/entitys/Bullet.java
+++ Tank/src/com/zk/tank/entitys/Bullet.java
@@ -11,13 +11,16 @@ import org.andengine.opengl.texture.region.TextureRegion;
 
 import android.content.Context;
 
+import com.zk.tank.components.Collision;
+import com.zk.tank.components.Explosion;
+import com.zk.tank.components.TiledMapRender;
 import com.zk.tank.constant.GameConstants;
 import com.zk.tank.interfaces.IAndEngine;
 
 /**
  * class mô tả 1 đối tượng {@link Bullet}
  * 
- * @author zk
+ * @author zk (ndviettien.zk@gmail.com)
  * @since 28/11/2012
  */
 public class Bullet implements GameConstants, IAndEngine {
@@ -29,19 +32,26 @@ public class Bullet implements GameConstants, IAndEngine {
 	private TextureRegion mRegion;
 	private Sprite mSprite;
 	
+	private int lvl;
 	private float speed;
 
 	//=================================================================================//
 	//									  CONSTRUCTORS
 	//=================================================================================//
+	
+	public Bullet(int lvl, float speed) {
+		this.lvl = lvl;
+		this.speed = speed;
+	}
 
 	//=================================================================================//
 	//									    METHODS
 	//=================================================================================//
+	
 	@Override
 	public void onCreateResource(Engine mEngine, Context context) {
 		BitmapTextureAtlasTextureRegionFactory.setAssetBasePath(ASSET_GRAPHICS);
-		mAtlas = new BitmapTextureAtlas(mEngine.getTextureManager(), 8, 8);
+		mAtlas = new BitmapTextureAtlas(mEngine.getTextureManager(), 5, 24);
 		mRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(mAtlas, context, "bullet.png", 0, 0);
 		mAtlas.load();
 	}
@@ -49,18 +59,103 @@ public class Bullet implements GameConstants, IAndEngine {
 	@Override
 	public void onCreateScene(Engine mEngine, Scene mScene) {
 		mSprite = new Sprite(0, 0, mRegion, mEngine.getVertexBufferObjectManager());
+		mSprite.setVisible(false);
+		mScene.attachChild(mSprite);
 	}
 
-	public void move(int direction, float pX, float pY, Scene mScene) {
+	/**
+	 * @param direction
+	 * @param pX
+	 * @param pY
+	 * @param mEngine
+	 * @param mScene
+	 */
+	public void move(final int direction, final float pX, final float pY, final Engine mEngine, final Scene mScene) {
+		
+		this.mSprite.setPosition(pX, pY);
+		this.mSprite.setVisible(true);
+		
 		this.mSprite.registerUpdateHandler(new TimerHandler(this.speed, new ITimerCallback() {
 			
 			@Override
 			public void onTimePassed(TimerHandler pTimerHandler) {
-				// TODO Auto-generated method stub
-				
+
+				// Kiểm tra tính hợp lệ của bước di chuyển
+				boolean isCollision = Collision.isCollision(mEngine, direction, mSprite, TiledMapRender.getRocks());
+
+				// Xử lý di chuyển với các hướng tương ứng
+				switch (direction) {
+
+				// hướng lên trên
+				case UP:
+					Bullet.this.mSprite.setRotation(UP);
+					// kiểm tra tính va chạm và vượt khỏi bản đồ
+					// nếu vi phạm, hiển thị chuỗi hình ảnh nổ
+					if (Bullet.this.mSprite.getY() - 8 <= 0 || isCollision) {
+						return;
+						}
+					else
+						// Di chuyển theo bước 4px
+						Bullet.this.mSprite.setPosition(Bullet.this.mSprite.getX(), Bullet.this.mSprite.getY() - SPEED_STEP);
+					break;
+				case RIGHT:
+					Bullet.this.mSprite.setRotation(RIGHT);
+					// kiểm tra tính va chạm và vượt khỏi bản đồ
+					// nếu vi phạm, hiển thị chuỗi hình ảnh nổ
+					if (Bullet.this.mSprite.getX() + TILED_HEIGHT >= 768 || isCollision)
+						return;
+					else
+						// Di chuyển theo bước 4px
+						Bullet.this.mSprite.setPosition(Bullet.this.mSprite.getX() + SPEED_STEP, Bullet.this.mSprite.getY());
+					break;
+				case DOWN:
+					Bullet.this.mSprite.setRotation(DOWN);
+					// kiểm tra tính va chạm và vượt khỏi bản đồ
+					// nếu vi phạm, hiển thị chuỗi hình ảnh nổ
+					if (Bullet.this.mSprite.getY() - 8  + TILED_HEIGHT >= 480 || isCollision)
+						return;
+					else
+						// Di chuyển theo bước 4px
+						Bullet.this.mSprite.setPosition(Bullet.this.mSprite.getX(), Bullet.this.mSprite.getY() + SPEED_STEP);
+					break;
+				case LEFT:
+					Bullet.this.mSprite.setRotation(LEFT);
+					// kiểm tra tính va chạm và vượt khỏi bản đồ
+					// nếu vi phạm, hiển thị chuỗi hình ảnh nổ
+					if (Bullet.this.mSprite.getX() <= 48 || isCollision)
+						return;
+					else
+						// Di chuyển theo bước 4px
+						Bullet.this.mSprite.setPosition(Bullet.this.mSprite.getX() - SPEED_STEP, Bullet.this.mSprite.getY());
+					break;
+				default:
+					break;
+				}
 			}
 		}));
+	}
+
+	public void setPoisition(float pX, float pY) {
 		this.mSprite.setPosition(pX, pY);
-		mScene.attachChild(mSprite);
+	}
+
+	//=================================================================================//
+	//									 GETTER & SETTER
+	//=================================================================================//
+
+	public float getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(float speed) {
+		this.speed = speed;
+	}
+	
+	public int getLvl() {
+		return lvl;
+	}
+
+	public void setLvl(int lvl) {
+		this.lvl = lvl;
 	}
 }
