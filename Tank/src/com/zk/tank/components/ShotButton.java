d3058712f739753bf35a1725f4e7f0bdb77e4fb6
zk <ndviettien.zk@gmail.com>
2012-11-29 15:42:00 +0700

    Tạo nút bắn lên màn hình, xử lý khi có sự kiện chạm

---
+++ Tank/src/com/zk/tank/components/ShotButton.java
@@ -0,0 +1,73 @@
+package com.zk.tank.components;
+
+import org.andengine.engine.Engine;
+import org.andengine.entity.scene.Scene;
+import org.andengine.entity.sprite.Sprite;
+import org.andengine.input.touch.TouchEvent;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
+import org.andengine.opengl.texture.region.TextureRegion;
+
+import android.content.Context;
+
+import com.zk.tank.constant.GameConstants;
+import com.zk.tank.entitys.Player;
+import com.zk.tank.interfaces.IAndEngine;
+
+public class ShotButton implements GameConstants, IAndEngine {
+
+	//=================================================================================//
+	//										FIELDS
+	//=================================================================================//
+	private BitmapTextureAtlas mAtlas;
+	private TextureRegion mRegion;
+	private Sprite mSprite;
+	
+	private Player player;
+
+	//=================================================================================//
+	//									  CONSTRUCTORS
+	//=================================================================================//
+	public ShotButton(Player player) {
+		this.player = player;
+	}
+	//=================================================================================//
+	//										METHODS
+	//=================================================================================//
+	
+	@Override
+	public void onCreateResource(Engine mEngine, Context context) {
+		BitmapTextureAtlasTextureRegionFactory.setAssetBasePath(ASSET_GRAPHICS);
+		mAtlas = new BitmapTextureAtlas(mEngine.getTextureManager(), 128, 128);
+		mRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(mAtlas, context, "on_fire_button.png", 0, 0);
+		mAtlas.load();
+	}
+
+	@Override
+	public void onCreateScene(final Engine mEngine, final Scene mScene) {
+		this.mSprite = new Sprite(CAMERA_WIDTH - mRegion.getWidth(), CAMERA_HEIGHT - mRegion.getHeight(), 
+				mRegion, mEngine.getVertexBufferObjectManager()) {
+			
+			/*
+			 * Phương thức xử lý khi có xự kiện chạm trên đối tượng
+			 */
+			@Override
+			public boolean onAreaTouched(final TouchEvent pSceneTouchEvent, final float pTouchAreaLocalX, final float pTouchAreaLocalY) {
+				int act = pSceneTouchEvent.getAction();
+				
+				switch (act) {
+				
+				// Đưa đối tượng Player vào trạng thái bắn nếu chạm xuống hoặc di chuyển trên đối tượng
+				case TouchEvent.ACTION_DOWN:
+				case TouchEvent.ACTION_MOVE:
+					ShotButton.this.player.fire(mEngine, mScene);
+					return true;
+				default:
+					return true;
+				}
+			}
+		};
+		
+		mScene.registerTouchArea(mSprite);
+	}
+}
