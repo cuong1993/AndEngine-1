c4780367ba0f84071c51622e30ba12175bc05c69
zk <ndviettien.zk@gmail.com>
2012-12-01 00:20:16 +0700

    Signed-off-by: zk <ndviettien.zk@gmail.com>

--- AndEngine test/src/com/truonghau/gunmap/interfaces/GameConstants.java
+++
@@ -1,9 +0,0 @@
-package com.truonghau.gunmap.interfaces;
-
-public interface GameConstants {
-	public static final int LAYER_COUNT = 10;
-	
-	public static final int LAYER_INI = 0;
-	public static final int LAYER_USER = LAYER_INI + 1;
-	public static final int LAYER_ROCKET_MENU = LAYER_USER + 1;
-}
